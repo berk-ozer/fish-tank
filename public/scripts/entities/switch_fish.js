4813a09f8ae9869c5bf38e2bbe59a4bfeba7d7a6
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-15 01:13:44 +0000

    Created a new fish called biteFish and changed images of all fish

--- public/scripts/entities/switch_fish.js
+++ public/scripts/entities/switch_fish.js
@@ -1,5 +1,10 @@
 class SwitchFish extends Fish {
 
+  constructor(options) {
+    super(options);
+    this.imageUri = '/images/grey_fish.png'
+  }
+
   onClick(event) {
     this.makeNewVelocity(50);
   }
